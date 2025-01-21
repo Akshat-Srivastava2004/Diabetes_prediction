71a76dcc272f55531464644b3c569e0223656514
Akshat-Srivastava2004 <akshatsri2004alld@gmail.com>
2025-01-21 14:37:35 +0530

    forredner

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 # Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
